46b862f31f773f8d1e92367eee16ba71846d2480
rairandiniz10 <rairamdiniz@hotmail.com>
2015-05-26 13:42:44 -0300

    Update Principal.cs

--- Principal.cs
+++ Principal.cs
@@ -2,6 +2,13 @@
 using System.Collections;
 
 public class Principal : MonoBehaviour {
+public GameObject X1,X2,X3,X4,X5,X6,X7,X8,X9;
+	public GameObject O1,O2,O3,O4,O5,O6,O7,O8,O9;
+	public GameObject player1,player2;
+	public int numero;
+	public bool contador1,contador2,contador3,contador4,contador5,contador6,contador7,contador8,contador9;
+	public bool contador21,contador22,contador23,contador24,contador25,contador26,contador27,contador28,contador29;
+	public AudioClip clik;
 	
 
 	void Start () {
